6bc4a95b02530dc6cb4544d56d38b9b210accb40
angelomedina <angelomedinabarraza@gmail.com>
2020-10-26 20:18:09 -0600

    first commit

---
+++ client/src/CompanyDetail.js
@@ -0,0 +1,34 @@
+import React, { Component } from 'react';
+import { JobList } from './JobList';
+//import { companies } from './fake-data';
+import { loadCompany } from './requets';
+
+export class CompanyDetail extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {company: null};
+  }
+  
+  async componentDidMount(){
+    const {companyId} = this.props.match.params;
+    const company = await loadCompany(companyId);
+    this.setState({
+      company
+    });
+  }
+
+  render() {
+    const {company} = this.state;
+    if(!company){
+      return null;
+    }
+    return (
+      <div>
+        <h1 className="title">{company.name}</h1>
+        <div className="box">{company.description}</div>
+        <h1 className="title is-5">Jobs at{company.name}</h1>
+        <JobList jobs={company.jobs} />
+      </div>
+    );
+  }
+}
